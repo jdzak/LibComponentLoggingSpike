2d9a91db15cb6f0898a00a22f062b46d0d5ba52b
John Dzak <john.dzak@gmail.com>
2012-10-27 15:23:27 -0500

    Initial Commit

---
+++ LibComponentLoggingSpike/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LibComponentLoggingSpike
+//
+//  Created by John Dzak on 10/27/12.
+//  Copyright (c) 2012 John Dzak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ LibComponentLoggingSpike/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LibComponentLoggingSpike
+//
+//  Created by John Dzak on 10/27/12.
+//  Copyright (c) 2012 John Dzak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
